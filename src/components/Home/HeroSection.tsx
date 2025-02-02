79ba2ff16dca76b57d7286897328ac0bc0fbc11e
md-asif-ullah <mdasifullah456@gmail.com>
2025-02-02 23:27:10 +0600

    update: refactor hero-section code

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -2,6 +2,94 @@ import Image from "next/image";
 import LargeDeviceHeader from "@/components/LargeDeviceHeader";
 import Button from "../Button";
 
+type ImageDataType = {
+  src: string;
+  alt: string;
+  height: number;
+  width: number;
+  className: string;
+};
+
+type smallScreenImagesType = {
+  src: string;
+  alt: string;
+  height: number;
+  width: number;
+  className: string;
+};
+
+const largeScreenImages: ImageDataType[] = [
+  {
+    src: "/images/hero-sectionGradient-image.png",
+    alt: "Hero Image",
+    height: 500,
+    width: 700,
+    className:
+      "xl:w-[85%] h-full hidden xl:block absolute top-0 left-0 right-0 bottom-0 z-10",
+  },
+  {
+    src: "/images/hero-sectionGradient-image2.png",
+    alt: "Hero Image",
+    height: 500,
+    width: 1000,
+    className:
+      "w-[80%] h-full hidden lg:block xl:hidden absolute top-0 left-0 right-0 bottom-0 z-10",
+  },
+  {
+    src: "/images/heroCornarImage.png",
+    alt: "Hero Image",
+    height: 50,
+    width: 50,
+    className: "w-auto h-auto xl:w-48 xl:h-48 absolute bottom-4 right-0 z-10",
+  },
+  {
+    src: "/images/homeImage.jpg",
+    alt: "Hero Image",
+    height: 500,
+    width: 500,
+    className: "w-2/3 h-full absolute top-0 hidden xl:block right-0 bottom-0",
+  },
+  {
+    src: "/images/homeImage2.jpg",
+    alt: "Hero Image",
+    height: 500,
+    width: 500,
+    className:
+      "w-2/3 h-full absolute top-0 hidden lg:block xl:hidden right-0 bottom-0",
+  },
+  {
+    src: "/images/heroBottom.png",
+    alt: "Hero Image",
+    height: 500,
+    width: 500,
+    className: "absolute right-0 left-0 bottom-0 z-10 w-full",
+  },
+];
+
+const smallScreenImages: smallScreenImagesType[] = [
+  {
+    src: "/images/sub-top.png",
+    alt: "Hero Image",
+    height: 500,
+    width: 500,
+    className: "absolute right-0 left-0 top-0 z-10 w-full",
+  },
+  {
+    src: "/images/heroBottom.png",
+    alt: "Hero Image",
+    height: 500,
+    width: 500,
+    className: "absolute right-0 left-0 bottom-0 z-10 w-full",
+  },
+  {
+    src: "/images/homeImage.jpg",
+    alt: "Hero Image",
+    height: 500,
+    width: 500,
+    className: "w-full",
+  },
+];
+
 const HeroSection = () => {
   return (
     <div className="xl:h-[85vh] lg:min-h-screen lg:h-[600px] h-full bg-gradient-to-r from-[#1a7ae9] via-[#1373e0] to-[#005ec8] lg:bg-none relative">
@@ -24,48 +112,16 @@ const HeroSection = () => {
             className="bg-[#fe8b53] border-[#fe8b53] hover:bg-[#fe8b53] hover:text-white mt-7"
           />
         </div>
-        <Image
-          src="/images/hero-sectionGradient-image.png"
-          alt="Hero Image"
-          height={500}
-          width={700}
-          className="xl:w-[85%] h-full hidden xl:block absolute top-0 left-0 right-0 bottom-0 z-10"
-        />
-        <Image
-          src="/images/hero-sectionGradient-image2.png"
-          alt="Hero Image"
-          height={500}
-          width={1000}
-          className="w-[80%] h-full hidden lg:block xl:hidden absolute top-0 left-0 right-0 bottom-0 z-10"
-        />
-        <Image
-          src="/images/heroCornarImage.png"
-          alt="Hero Image"
-          height={50}
-          width={50}
-          className="w-auto h-auto xl:w-48 xl:h-48 absolute bottom-4 right-0 z-10"
-        />
-        <Image
-          src="/images/homeImage.jpg"
-          alt="Hero Image"
-          height={500}
-          width={500}
-          className="w-2/3 h-full absolute top-0 hidden xl:block right-0 bottom-0"
-        />
-        <Image
-          src="/images/homeImage2.jpg"
-          alt="Hero Image"
-          height={500}
-          width={500}
-          className="w-2/3 h-full absolute top-0 hidden lg:block xl:hidden right-0 bottom-0"
-        />
-        <Image
-          src="/images/heroBottom.png"
-          alt="Hero Image"
-          height={500}
-          width={500}
-          className="absolute right-0 left-0 bottom-0 z-10 w-full"
-        />
+        {largeScreenImages.map((image, index) => (
+          <Image
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            height={image.height}
+            width={image.width}
+            className={image.className}
+          />
+        ))}
       </div>
 
       {/* mobile design */}
@@ -86,27 +142,16 @@ const HeroSection = () => {
       </section>
 
       <div className="relative mt-10 lg:hidden">
-        <Image
-          src="/images/sub-top.png"
-          alt="Hero Image"
-          height={500}
-          width={500}
-          className="absolute right-0 left-0 top-0 z-10 w-full"
-        />
-        <Image
-          src="/images/heroBottom.png"
-          alt="Hero Image"
-          height={500}
-          width={500}
-          className="absolute right-0 left-0 bottom-0 z-10 w-full"
-        />
-        <Image
-          src="/images/homeImage.jpg"
-          alt="Hero Image"
-          height={500}
-          width={500}
-          className="w-full"
-        />
+        {smallScreenImages.map((image: smallScreenImagesType, index) => (
+          <Image
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            height={image.height}
+            width={image.width}
+            className={image.className}
+          />
+        ))}
       </div>
     </div>
   );
